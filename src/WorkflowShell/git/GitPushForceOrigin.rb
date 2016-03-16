ba520c5f5a53d46b78a30b68f64bc9a2ba74d61a
Mac Champion <mac.champion@cerner.com>
2016-03-16 16:01:16 -0500

    Beginning-OptionsParser-Work

--- src/WorkflowShell/git/GitPushForceOrigin.rb
+++ src/WorkflowShell/git/GitPushForceOrigin.rb
@@ -1,12 +1,34 @@
 require_relative '../models/Command'
-
+#puts "gpfo: "
 class GitPushForceOrigin < Command
-  @command_string='gpfo'
+  @command_string
+  @command_description
+
   def initialize
-    super(@command_string)
+    @command_string='gpfo'
+    @command_description='Force pushes the current branch into a remote branch of the same name.'
   end
 
-  def stuff
-    puts("I guess it worked.")
+  def run_command(arguments)
+    parser_components = parse_options(arguments)
+    parser_components.basic_options
+
+    # parser_components.options_parser
+    parser_components.leftover_arguments
+
+    options = OpenStruct.new
+    options.verbose = false
+
+    specific_options_parser = OptionParser.new do |opts|
+      opts.separator ""
+      opts.separator "There are no specific options for this command."
+
+      if parser_components.basic_options.help
+        puts opts
+        exit
+      end
+    end
+
+    puts "DO STUFF"
   end
 end
--- src/WorkflowShell/models/Command.rb
+++ src/WorkflowShell/models/Command.rb
@@ -1,26 +1,55 @@
 
 class Command
-  @command_string
-  def initialize(command_name)
-    @command_string=command_name
-  end
 
+  # These are intended to be superclass methods. Hopefully each subclass will define these variables.
   def get_command_string
     @command_string
   end
-
-
-
-  def inherited other
-    super if defined? super
-  ensure
-    ( @subclasses ||= [] ).push(other).uniq!
+  def get_command_description
+    @command_description
   end
 
-  def subclasses
-    @subclasses ||= []
-    @subclasses.inject( [] ) do |list, subclass|
-      list.push(subclass, *subclass.subclasses)
+  # Main body of commands. If this instance of this method is reached, something went wrong.
+  def run_command(arguments)
+    raise "Specified command did not implement 'run_command' method."
+  end
+
+  def parse_options(arguments)
+    options = OpenStruct.new
+    options.verbose = false
+    options.help = true
+
+    basic_options_parser = OptionParser.new do |opts|
+      opts.banner = "Usage: " + @command_string
+      opts.separator ""
+      opts.separator "Basic options:"
+
+      # Boolean switch.
+      opts.on("-v", "--verbose", "Run verbosely") do
+        options.verbose = true
+      end
+
+      opts.on_tail("-h", "--help", "Show this message") do
+        options.help = true
+        puts opts
+      end
     end
+
+    # This will call the parser and make it put all basic options into basic_options
+    # It will remove all options that it interacts with.
+    basic_options_parser.parse!(arguments)
+
+    # OpenStruct allows you to basically create a POJO-esque map-type object on the fly.
+    parser_components = OpenStruct.new
+    parser_components.basic_options = options
+    # parser_components.options_parser = basic_options_parser
+    parser_components.leftover_arguments = arguments
+    parser_components
+  end
+
+  # This method will return all subclasses of this class
+  # https://stackoverflow.com/questions/3676274/how-to-get-all-class-names-in-a-namespace-in-ruby
+  def self.descendants
+    ObjectSpace.each_object(Class).select { |klass| klass < self }
   end
 end
