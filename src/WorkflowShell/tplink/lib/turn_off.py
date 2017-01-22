eca7af5d1f7df0da904cff4e92750092ffe28426
Your Name <mac@mac.mac>
2017-01-21 19:54:18 -0500

    uplift python

--- src/WorkflowShell/tplink/lib/turn_off.py
+++ src/WorkflowShell/tplink/lib/turn_off.py
@@ -2,6 +2,7 @@
 # taken from https://github.com/natefox/tplink-hs100
 
 import socket
+import codecs
 
 IPADDR = '192.168.1.209'
 PORTNUM = 9999
@@ -9,9 +10,9 @@ PORTNUM = 9999
 # FROM ON TO OFF
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((IPADDR, PORTNUM))
-data = '0000002ad0f281f88bff9af7d5ef94b6c5a0d48bf99cf091e8b7c4b0d1a5c0e2d8a381f286e793f6d4eedea3dea3'.decode('hex')
+data = codecs.decode('0000002ad0f281f88bff9af7d5ef94b6c5a0d48bf99cf091e8b7c4b0d1a5c0e2d8a381f286e793f6d4eedea3dea3','hex')
 s.send(data)
-data = '0000002dd0f281f88bff9af7d5ef94b6c5a0d48bf99cf091e8b7c4b0d1a5c0e2d8a381e496e4bbd8b7d3b694ae9ee39ee3'.decode('hex')
+data = codecs.decode('0000002dd0f281f88bff9af7d5ef94b6c5a0d48bf99cf091e8b7c4b0d1a5c0e2d8a381e496e4bbd8b7d3b694ae9ee39ee3','hex')
 s.send(data)
 s.close
 
